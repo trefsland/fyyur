18f6b8c77332f0b5bf28fc65de27cd331a9a31bd
TylerRefsland <tyler.refsland@gmail.com>
2020-12-29 11:12:24 -0500

    fixed issue preventing entry of multiple genres for a given venue

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    genres = db.Column(db.ARRAY(db.String))
+    genres = db.Column(db.ARRAY(db.String()))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
@@ -58,8 +58,8 @@ class Venue(db.Model):
         backref=db.backref('venues', lazy=True))
     #past_shows =
     #upcoming_shows =
-    past_shows_count = db.Column(db.Integer)
-    upcoming_shows_count = db.Column(db.Integer)
+    #past_shows_count = db.Column(db.Integer)
+    #upcoming_shows_count = db.Column(db.Integer)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -72,15 +72,15 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     website = db.Column(db.String(500))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String(120))
+    seeking_description = db.Column(db.String())
     #past_shows =
     #upcoming_shows =
-    past_shows_count = db.Column(db.Integer)
-    upcoming_shows_count = db.Column(db.Integer)
+    #past_shows_count = db.Column(db.Integer)
+    #upcoming_shows_count = db.Column(db.Integer)
 
 # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -257,7 +257,7 @@ def create_venue_submission():
             state = request.form['state'],
             address = request.form['address'],
             phone = request.form['phone'],
-            genres = request.form['genres'],
+            genres = request.form.getlist('genres'),
             facebook_link = request.form['facebook_link'])
         db.session.add(venue)
         db.session.commit()
@@ -267,9 +267,8 @@ def create_venue_submission():
     finally:
         db.session.close()
     if error:
-        # TODO: on unsuccessful db insert, flash an error instead.
+        # on unsuccessful db insert, flash an error instead.
         flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
-        # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
         #abort(500)
     else:
         # on successful db insert, flash success
